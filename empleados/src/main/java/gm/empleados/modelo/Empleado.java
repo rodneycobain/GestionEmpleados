ca7d619daddc85c0dca4c1e22ecbf6634c9b6301
YISUSBG <jesusbersabegonzalez@gmail.com>
2025-08-20 19:49:12 +0200

    fourth Commit Conexion con Base de datos Mysql

---
+++ empleados/src/main/java/gm/empleados/modelo/Empleado.java
@@ -0,0 +1,27 @@
+package gm.empleados.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idEmpleado;
+    String nombreEmpleado;
+    String departamento;
+    Double sueldo;
+
+
+
+
+}
