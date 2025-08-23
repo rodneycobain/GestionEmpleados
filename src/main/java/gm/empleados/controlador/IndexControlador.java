1e2d7118cc63c69e1dd453a65d99e710eae8d333
YISUSBG <jesusbersabegonzalez@gmail.com>
2025-08-23 17:21:20 +0200

    Thrityth Commit Fucionamiento Boton Agregar

--- src/main/java/gm/empleados/controlador/IndexControlador.java
+++ src/main/java/gm/empleados/controlador/IndexControlador.java
@@ -2,11 +2,13 @@ package gm.empleados.controlador;
 
 import gm.empleados.modelo.Empleado;
 import gm.empleados.servicio.EmpleadoServicio;
+import jakarta.servlet.http.HttpServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
@@ -28,5 +30,18 @@ public class IndexControlador {
         //Compartimos el modelo con la vista
         modelo.put("empleados", empleados);
         return "index"; //index.jsp
+    }
+
+    @RequestMapping(value = "/agregar", method = RequestMethod.GET)
+    public String mostrarAgregar(){
+        return "agregar"; //agregar.jsp
+    }
+
+    @RequestMapping(value="/agregar", method = RequestMethod.POST)
+    public String agregar(@ModelAttribute("empleadoForma") Empleado empleado){
+        logger.info("Empleado a agregar: " + empleado);
+        empleadoServicio.guardarEmpleado(empleado);
+        return "redirect:/";//redirige al path "/"
+
     }
 }
